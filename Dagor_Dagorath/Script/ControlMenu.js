0ee1f5aed3ace53fadc027d3f477e74462a678ae
JorgeMartinezSepulveda <actionjorge@gmail.com>
2018-10-10 17:51:23 +0200

    Pruebas de animacion, provisionales

--- Dagor_Dagorath/Script/ControlMenu.js
+++ Dagor_Dagorath/Script/ControlMenu.js
@@ -1,6 +1,7 @@
 var DagorDagorath = DagorDagorath || {};
 var ventanaControles;
 var button;
+var tween;
 
 DagorDagorath.ControlMenu = function(){};
 
@@ -11,10 +12,23 @@ DagorDagorath.ControlMenu.prototype = {
 
     //background
     this.background = this.game.add.tileSprite(0, 0, 1000, 667, 'background3');
-      	
+
   	ventanaControles = this.game.add.sprite(100,58, 'controles');
     ventanaControles.width = 800;
     ventanaControles.height = 550;
+
+    /*
+    var w = this.game.width - ventanaControles.width;
+    var h = this.game.height - ventanaControles.height;
+
+    tween = this.game.add.tween(ventanaControles).to( { x: [ w, w, 0, 0 ], y: [ 0, h, h, 0 ] }, 4000, "Sine.easeInOut", true, -1, false);
+  */
+
+    tween = this.game.add.tween(ventanaControles.scale).to({x: 0.95, y: 0.95}, 2000, Phaser.Easing.Linear.None);
+    tween.start();
+    tween = this.game.add.tween(ventanaControles).to({x: 70, y: 25}, 2000, Phaser.Easing.Linear.None);
+    tween.start();
+
 
     button = this.game.add.button(210, 137, 'BotonRetroceso', this.actionOnClick, this,1,0);
     button.width = 85;
